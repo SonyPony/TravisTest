485f49d066384b8723f516e22a63f3ad2737165d
Sony Nguyen <son.nguyen@olc.cz>
2015-10-18 12:15:34 +0200

    exclude generated files

---
+++ utils/after_success_linux.sh
@@ -0,0 +1,24 @@
+exclude_folders=()
+
+# Exclude generated files.
+exclude_regexs=(
+    '.*/moc_.*'
+    '.*\.moc$'
+    '.*_automoc\..*'
+    '.*/ui_.*'
+    '.*/qrc_.*'
+    '.*CMake.*'
+    '.*/tests/.*'
+)
+
+arguments=()
+
+for file in "${exclude_files[@]}"; do
+    arguments+=(--exclude "$file")
+done
+
+for regex in "${exclude_regexs[@]}"; do
+    arguments+=(--exclude-pattern "$regex")
+done
+
+coveralls "${arguments[@]}"
