ce3ba964192ca563d363f907def77884b6cf8f36
Sony Nguyen <sirarseus@gmail.com>
2015-10-18 19:13:11 +0200

    correct fix

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     if(QApplication::arguments().contains("--test"))
-        return 0;
+        return result;
     else
         return app.exec();
 }
--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -7,5 +7,5 @@ void Tests::trueTest()
 
 void Tests::falseTest()
 {
-    QCOMPARE("Ahojky", "Ahojkdy");
+    QCOMPARE("Ahojky", "Ahojky");
 }
